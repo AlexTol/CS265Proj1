f0a51db9a0737fdd7903989bec3f9834de5500e2
Alex <atol514@gmail.com>
2020-09-24 22:27:02 -0700

    implemented base64 decode

--- encryption.h
+++ encryption.h
@@ -1,6 +1,13 @@
 #include <string.h>
 #include <math.h> //need gcc main.c -lm   to link math library
 
+int base64DecodeSize(char str[])
+{
+    int len = strlen(str);
+
+    return (3 * (len/4));
+}
+
 int base64EncodeSize(char str[])
 {
     int len = strlen(str);
@@ -8,10 +15,34 @@ int base64EncodeSize(char str[])
     return (4 * ((len +2)/ 3));
 }
 
-int * charToBin(char c)
+int * decToBin(int d, int size)
 {
-        static int bin[8];
-        for(int i = 0; i < 8; i++)
+    int *bin = malloc(sizeof(int) * size);
+    
+    int mNum = d;
+    int spot = size-1;
+    for(int i = 0; i < size; i++)
+    {
+        if(pow(2,spot) > mNum)
+        {
+            bin[i] = 0;
+        }
+        else
+        {
+            bin[i] = 1;
+            mNum = mNum - pow(2,spot);
+        }
+
+        spot--;
+    }
+    
+    return bin;
+}
+
+int * charToBin(char c,int size)
+{
+        int *bin = malloc(sizeof(int) * size);
+        for(int i = 0; i < size; i++)
         {
             bin[i] = ( ((c << i) & 128)/128);
         }
@@ -94,7 +125,7 @@ char *encodeBase64(char str[])
         int *buf2;
         int *buf3;
 
-        buf1 = charToBin(str[i]);
+        buf1 = charToBin(str[i],8);
         int pad1[8];
         intArrCpy(pad1,buf1,8);
         //free(buf1);
@@ -106,7 +137,7 @@ char *encodeBase64(char str[])
         }
         else
         {
-            buf2 = charToBin(str[i+1]);
+            buf2 = charToBin(str[i+1],8);
         }
 
         int pad2[8];
@@ -120,7 +151,7 @@ char *encodeBase64(char str[])
         }
         else
         {
-            buf3 = charToBin(str[i+2]);
+            buf3 = charToBin(str[i+2],8);
         }
 
         int pad3[8];
@@ -137,6 +168,12 @@ char *encodeBase64(char str[])
         intArrCpy(padStr,temp3,24);
         free(temp3);
 
+        for(int j=0; j < 24; j++)
+        {
+            printf("%d",padStr[j]);
+        }
+        printf("\n");
+
         int group1[6];
         int group2[6];
         int group3[6];
@@ -146,6 +183,7 @@ char *encodeBase64(char str[])
         int count2 = 0;
         int count3 = 0;
         int count4 = 0;
+
         for(int i = 0; i < 24; i++)
         {
             if(count1 < 6)
@@ -190,12 +228,137 @@ char *encodeBase64(char str[])
     return newStr;
 }
 
-char *decodeBase64(char str[])
+int returnSpace(char c)
 {
-    char * c;
-    return c;
+    const char base64tab[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    for(int i = 0; i <64; i++)
+    {
+        if(c == base64tab[i])
+        {
+            return i;
+        }
+    }
+
+    return -1;
 }
 
+char *decodeBase64(char str[])
+{
+    int regSize = base64DecodeSize(str);
+    char *newStr = malloc(regSize+1);
+
+
+    int pos = 0;
+    for(int i = 0; i < strlen(str); i = i + 4)
+    {
+        int s1 = returnSpace(str[i]);
+        int s2 = returnSpace(str[i+1]);
+        int s3 = returnSpace(str[i+2]);
+        int s4 = returnSpace(str[i+3]);
+
+        int ignore3 = 0;
+        int ignore4 = 0;
+
+        int *buf1;
+        int *buf2;
+        int *buf3;
+        int *buf4;
+
+        buf1 = decToBin((char) s1,6);
+        int pad1[6];
+        intArrCpy(pad1,buf1,6);
+
+        buf2 = decToBin(s2,6);
+        int pad2[6];
+        intArrCpy(pad2,buf2,6);
+
+        int pad3[6];
+        if(s3 == -1)
+        {
+            ignore3 = 1;
+        }
+        else
+        {
+            buf3 = decToBin(s3,6);
+            intArrCpy(pad3,buf3,6);
+        }
+
+        int pad4[6];
+        if(s4 == -1)
+        {
+            ignore4 = 1;
+        }
+        else
+        {
+            buf4 = decToBin(s4,6);
+            intArrCpy(pad4,buf4,6);
+        }
+
+        int *temp1 = concatArrs(pad1,pad2,6,6);
+        int  temp2[12];
+        intArrCpy(temp2,temp1,12);
+        free(temp1);
+
+        int *temp3 = concatArrs(temp2,pad3,12,6);
+        int temp4[18];
+        intArrCpy(temp4,temp3,18);
+        free(temp3);
+
+        int *temp5 = concatArrs(temp4,pad4,18,6);
+        int padStr[24];
+        intArrCpy(padStr,temp5,24);
+        free(temp5);
+
+        for(int j=0; j < 24; j++)
+        {
+            printf("%d",padStr[j]);
+        }
+        printf("\n");
+
+        int group1[8];
+        int group2[8];
+        int group3[8];
+    
+        int count1 = 0;
+        int count2 = 0;
+        int count3 = 0; 
+
+        for(int i =0; i < 24; i++)
+        {
+            if(count1 < 8)
+            {
+                group1[count1] = padStr[i];
+                count1++;
+            }
+            else if(count2 < 8)
+            {
+                group2[count2] = padStr[i];
+                count2++;
+            }
+            else if(count3 < 8)
+            {
+                group3[count3] = padStr[i];
+                count3++;
+            }
+        }
+
+        
+
+        int val1 = binArrToDec(group1,8);
+        int val2 = binArrToDec(group2,8);
+        int val3 = binArrToDec(group3,8);
+
+        newStr[pos] = val1;
+        newStr[pos+1] = (ignore3  == 1) ? ' ' : val2;
+        newStr[pos+2] = (ignore4  == 1) ? ' ' : val3;
+
+        pos +=3;
+    }
+    return newStr;
+}
+
+//todo rename this to decryption
 void encryption(char str[])
 {
 
