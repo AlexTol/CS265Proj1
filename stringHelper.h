8a628399d91f8a21a49c63d8d30e9a882e84b8b8
AlexTol <atol514@gmail.com>
2020-10-06 13:41:09 -0700

    added stringhelper file

---
+++ stringHelper.h
@@ -0,0 +1,28 @@
+#ifndef _STRINGHELPERH_
+#define _STRINGHELPERH_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *stripNewLine(char *str)
+{
+    int len = strlen(str);
+    char *newArr = (char *)malloc(len+1);
+
+    for(int i = 0; i < len;i++)
+    {
+        if(i == (len-1) && str[len-1] == '\n')
+        {
+            newArr[i] = '\0';
+        }
+        else
+        {
+            newArr[i] = str[i];
+        }
+    }
+    newArr[len] = '\0';
+    return newArr;
+}
+
+#endif
